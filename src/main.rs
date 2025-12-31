f008ee19ebf3942a15d5f924e1c9b6bd65652571
Kanji Tanaka <sumireminami@gmail.com>
2025-12-31 21:08:03 +0900

    Implement Kafka message

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use codecrafters_kafka as kfk;
+use kfk::{Header, Message};
+
+use bytes::Bytes;
+use std::io::Write;
 use std::net::TcpListener;
 
 fn main() {
@@ -11,8 +16,10 @@ fn run() -> Result<(), Box<dyn std::error::Error>> {
     let listener = TcpListener::bind("127.0.0.1:9092")?;
 
     for stream in listener.incoming() {
-        let _stream = stream?;
-        println!("accepted new connection");
+        let mut stream = stream?;
+        let msg = Message::new(Header::new_v0(7));
+        let bytes: Bytes = msg.into();
+        stream.write_all(&bytes)?;
     }
 
     Ok(())
