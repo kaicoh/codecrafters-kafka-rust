286ed3b41874ba327a4b40f486367939b9ea837e
Kanji Tanaka <sumireminami@gmail.com>
2025-12-31 20:30:31 +0900

    Init project

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,19 @@
-#![allow(unused_imports)]
 use std::net::TcpListener;
 
 fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
+    if let Err(e) = run() {
+        eprintln!("{e}");
+        std::process::exit(1);
+    }
+}
 
-    // TODO: Uncomment the code below to pass the first stage
-    //
-    // let listener = TcpListener::bind("127.0.0.1:9092").unwrap();
-    //
-    // for stream in listener.incoming() {
-    //     match stream {
-    //         Ok(_stream) => {
-    //             println!("accepted new connection");
-    //         }
-    //         Err(e) => {
-    //             println!("error: {}", e);
-    //         }
-    //     }
-    // }
+fn run() -> Result<(), Box<dyn std::error::Error>> {
+    let listener = TcpListener::bind("127.0.0.1:9092")?;
+
+    for stream in listener.incoming() {
+        let _stream = stream?;
+        println!("accepted new connection");
+    }
+
+    Ok(())
 }
